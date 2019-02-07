23e450aabc129d1c1bd252bfe6b6d2fbb97a57b8
Allenl19LSRHS <47216743+Allenl19LSRHS@users.noreply.github.com>
2019-02-07 08:53:38 -0500

    Added to git

---
+++ Airplane.java
@@ -0,0 +1,26 @@
+
+public class Airplane {
+	int numEngines;
+	int airspeed;
+	int altitude;
+	int heading;
+	String type;
+	
+	// Maybe add a string to represent type of plane or type of engine
+	
+	public Airplane(int numEngines, String type) {
+		// Set the default state for the plane (airspeed, heading, and altitude)
+	}
+	
+	void changeHeading(int newHeading) {
+		// change the heading to the new heading
+	}
+	
+	void changeSpeed(int newAirspeed) {
+		// Speed up or slow down to new airspeed
+	}
+	
+	void changeAlt(int newAlt) {
+		// Climb or descend to new altitude
+	}
+}
